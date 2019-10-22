9c2539453743a0013fae9889b9682e3405cf37ce
EC2 Default User <ec2-user@ip-172-31-37-207.us-east-2.compute.internal>
2019-10-22 17:38:53 +0000

    generate model for comments section

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence:true
   belongs_to :user
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true,
             length: { maximum: 20 } 
   has_many :posts
+  has_many :comments
 end
